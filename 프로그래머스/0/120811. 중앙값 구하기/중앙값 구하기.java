74faa8d889973544226c4be0e05617b9a3b29f7f
Yunjae Lee <135799134+itsjaeya@users.noreply.github.com>
2024-09-25 00:40:25 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.52 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        
+        int length = array.length;
+        int answer = array[length / 2];
+        
+        return answer;
+    }
+}
